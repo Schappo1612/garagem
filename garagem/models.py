9f623d5872a0089d98ec271d645371dd720a06c5
PhelipiM <ggizmeuamigo@gmail.com>
2023-03-13 16:53:00 -0300

    2°parte

--- garagem/models.py
+++ garagem/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Marca(models.Model):
+    nome = models.CharField(max_length=50)
+    nacionalidade = models.URLField(null=True, blank=True, max_length=50)
+    def __str__(self):
+        return self.marca.upper()
