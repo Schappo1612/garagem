34c972839b00f0dda740a704bbe7c1761ad5fb4c
Schappo <gustavoschappo1612@gmail.com>
2023-03-20 15:56:38 -0300

    3parte

--- garagem/models.py
+++ garagem/models.py
@@ -5,3 +5,9 @@ class Marca(models.Model):
     nacionalidade = models.URLField(null=True, blank=True, max_length=50)
     def __str__(self):
         return self.marca.upper()
+
+class Categoria(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
