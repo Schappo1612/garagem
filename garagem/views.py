63b527bd9ac1cf50b1b5d5bc437f632a8662a89e
Schappo1612 <gustavoschappo1612@gmail.com>
2023-05-12 08:22:41 -0300

    3° parte

--- garagem/views.py
+++ garagem/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+
+from garagem.models import Categoria, Marca, Acessorio, Cor, Veiculo
+from garagem.serializers import CategoriaSerializer, MarcaSerializer, AcessorioSerializer, CorSerializer, VeiculoSerializer
+
+class CategoriaViewSet(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+
+class MarcaViewSet(ModelViewSet):
+    queryset = Marca.objects.all()
+    serializer_class = MarcaSerializer
+
+class AcessorioViewSet(ModelViewSet):
+    queryset = Acessorio.objects.all()
+    serializer_class = AcessorioSerializer
+
+class CorViewSet(ModelViewSet):
+    queryset = Cor.objects.all()
+    serializer_class = CorSerializer
+
+class VeiculoViewSet(ModelViewSet):
+    queryset = Veiculo.objects.all()
+    serializer_class = VeiculoSerializer
