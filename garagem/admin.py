712b476c070ef1e0977f2694b584d15ae9be58fd
schappo1612 <gustavoschappo1612@gmail.com>
2023-05-19 07:55:36 -0300

    admin concluido

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 
-from .models import Marca
+from garagem.models import Marca, Categoria, Acessorio, Cor, Veiculo
 
 admin.site.register(Marca)
+admin.site.register(Categoria)
+admin.site.register(Acessorio)
+admin.site.register(Cor)
+admin.site.register(Veiculo)
+
+
+
--- garagem/views.py
+++ garagem/views.py
@@ -1,17 +1,12 @@
-from django.shortcuts import render
-
 from rest_framework.viewsets import ModelViewSet
 
-from garagem.models import Categoria, Marca, Acessorio, Cor, Veiculo
-from garagem.serializers import CategoriaSerializer, MarcaSerializer, AcessorioSerializer, CorSerializer, VeiculoSerializer
+from garagem.models import Acessorio,Categoria,Cor,Marca,Veiculo 
+from garagem.serializers import AcessorioSerializer,CategoriaSerializer,CorSerializer,MarcaSerializer,VeiculoSerializer,VeiculoDetailSerializer,VeiculoListSerializer
 
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
 
-class MarcaViewSet(ModelViewSet):
-    queryset = Marca.objects.all()
-    serializer_class = MarcaSerializer
 
 class AcessorioViewSet(ModelViewSet):
     queryset = Acessorio.objects.all()
@@ -21,6 +16,17 @@ class CorViewSet(ModelViewSet):
     queryset = Cor.objects.all()
     serializer_class = CorSerializer
 
+class MarcaViewSet(ModelViewSet):
+    queryset = Marca.objects.all()
+    serializer_class = MarcaSerializer
+
 class VeiculoViewSet(ModelViewSet):
     queryset = Veiculo.objects.all()
     serializer_class = VeiculoSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return VeiculoListSerializer
+        elif self.action == "retrieve":
+            return VeiculoDetailSerializer
+        return VeiculoSerializer
